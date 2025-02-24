e4e27b9d77a70313024ae9905cd3fcba84d02c87
Nimira43 <n.rabone72@aol.com>
2025-02-24 04:25:19 +0000

    Set up root route and returning HTML

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,16 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <h1>Serapium</h1>
+      <hr />
+      <p>Inventory Management System</p>
+    </div>
+  `)
+})
+
 app.listen(5000, () => {
   console.log(`Server listening on Port 5000`)
 })
