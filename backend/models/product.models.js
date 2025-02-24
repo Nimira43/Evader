bfc2f828bcb4a1b114483106c0add6a86f4ed057
Nimira43 <n.rabone72@aol.com>
2025-02-24 05:40:02 +0000

    Set up a basic product schema

--- backend/models/product.models.js
+++ backend/models/product.models.js
@@ -2,6 +2,15 @@ import mongoose from 'mongoose'
 
 const productSchema = new mongoose.Schema({
   name: {
-    
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
   }
 })
