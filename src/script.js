1f8d4b7947f55eb983ee34b306a77eed39487509
Nimira43 <n.rabone72@aol.com>
2025-03-07 02:54:01 +0000

    Updated files.

--- src/script.js
+++ src/script.js
@@ -3,48 +3,47 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff4500 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
 window.addEventListener('resize', () => {
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+)
+camera.position.z = 5
 scene.add(camera)
 
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-const clock = new THREE.Clock()
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff4500 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
-    controls.update()
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(tick)
+const animate = () => {
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(animate)
 }
 
-tick()
+animate()
