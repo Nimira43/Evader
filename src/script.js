e63724e8fa48e0515238391c4ebf36b231183d92
Nimira43 <n.rabone72@aol.com>
2025-03-07 02:37:28 +0000

    Brought in favicon.

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0xff4500 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
